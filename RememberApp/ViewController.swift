57262265801f6f6ea8063bf6a71fb6948a70333f
Darko <darko.stojanov@yahoo.com>
2020-08-05 00:59:47 +0200

    Initial Commit

---
+++ RememberApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RememberApp
+//
+//  Created by Darko Stojanov on 8/5/20.
+//  Copyright © 2020 Darko Stojanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
